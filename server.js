ad790896e9aaabce916bd15bcb9d5e9dd41001a1
Kenneth Ekandem <kennyekandem@gmail.com>
2021-03-11 14:14:22 +0100

    update

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+
+    socket.on('testSocket', () => {
+        io.emit('testSocket', () => {
+
+        });
+    });
+
+});
+
+http.listen(process.env.PORT, () => {
+    console.log("Listening on port :5555");
+});
